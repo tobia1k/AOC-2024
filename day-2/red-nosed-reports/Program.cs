53f4e1204724eb5edac39ce05c0ad0e045326259
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-12-05 21:30:13 -0500

    Complete Day 2 Part 1

--- day-2/red-nosed-reports/Program.cs
+++ day-2/red-nosed-reports/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace day2;
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        try 
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string line;
+            int safeCount = 0;
+            while((line = sr.ReadLine()) != null)
+            {
+                int[] report = line.Split(' ').Select(int.Parse).ToArray();
+                bool increasing = true;
+                bool safe = true;
+                if (report[0] > report[1]) {
+                    increasing = false;
+                }
+                for (int i = 1; i < report.Length; i++) {
+                    int diff = report[i] - report[i-1];
+                    if (increasing && report[i - 1] < report[i] && diff <= 3 && diff >= 1) {
+                        continue;
+                    }
+                    else if (!increasing && report[i - 1] > report[i] && diff >= -3 && diff <= -1) {
+                        continue;
+                    }
+                    else {
+                        safe = false;
+                        break;
+                    }
+                }
+                if (safe) {
+                    safeCount++;
+                }
+                
+            }
+            Console.WriteLine(safeCount);
+        }
+        catch(Exception e) 
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Executing finally block.");
+        }
+    }
+}

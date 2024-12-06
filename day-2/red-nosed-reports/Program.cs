3d6b2a45f160d44e3fd8d0aefc81381ab3c13189
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-12-05 21:32:28 -0500

    Tweak formatting

--- day-2/red-nosed-reports/Program.cs
+++ day-2/red-nosed-reports/Program.cs
@@ -9,32 +9,44 @@ class Program
             StreamReader sr = new StreamReader("input.txt");
             string line;
             int safeCount = 0;
+            
             while((line = sr.ReadLine()) != null)
             {
                 int[] report = line.Split(' ').Select(int.Parse).ToArray();
                 bool increasing = true;
                 bool safe = true;
-                if (report[0] > report[1]) {
+
+                if (report[0] > report[1]) 
+                {
                     increasing = false;
                 }
-                for (int i = 1; i < report.Length; i++) {
+
+                for (int i = 1; i < report.Length; i++)
+                {
                     int diff = report[i] - report[i-1];
-                    if (increasing && report[i - 1] < report[i] && diff <= 3 && diff >= 1) {
+
+                    if (increasing && report[i - 1] < report[i] && diff <= 3 && diff >= 1) 
+                    {
                         continue;
                     }
-                    else if (!increasing && report[i - 1] > report[i] && diff >= -3 && diff <= -1) {
+                    else if (!increasing && report[i - 1] > report[i] && diff >= -3 && diff <= -1) 
+                    {
                         continue;
                     }
-                    else {
+                    else 
+                    {
                         safe = false;
                         break;
                     }
                 }
-                if (safe) {
+
+                if (safe) 
+                {
                     safeCount++;
                 }
                 
             }
+
             Console.WriteLine(safeCount);
         }
         catch(Exception e) 

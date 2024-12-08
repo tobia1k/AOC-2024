d524b2806aaf7ad2f7d2a947355871d6d373ffe3
tobia1k <113366743+tobia1k@users.noreply.github.com>
2024-12-08 13:15:22 -0500

    Extract all forwards and backwards "XMAS"s

--- day-4/ceres-search/Program.cs
+++ day-4/ceres-search/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+namespace day4;
+
+class Program
+{
+    static void Main(string[] args) 
+    {
+        try
+        {
+            Regex regex = new Regex(@"XMAS|SAMX");
+            StreamReader sr = new StreamReader("input.txt");
+            string? line;
+            while((line = sr.ReadLine()) != null) {
+                MatchCollection matches = regex.Matches(line);
+                foreach (Match match in matches) {
+                    Console.WriteLine(match);
+                }
+            }
+        }
+        catch (Exception e) 
+        {
+            Console.WriteLine("Exception: " + e);
+        }
+        finally 
+        {
+            Console.WriteLine("Executing finally block");
+        }
+    }
+}
